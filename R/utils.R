43eb4f7797d00d56d9af01bc23de663269a7e22f
Luise Quoß <luise.quoss@idiv.de>
2021-04-02 10:16:58 +0200

    added ebv_i_type_ot and ebv_i_type_raster

--- R/utils.R
+++ R/utils.R
@@ -74,8 +74,157 @@ ebv_i_type_r <- function(type.long){
   return(type.short)
 }
 
-# ebv_i_type_ot
-# ebv_i_type_raster
+ebv_i_type_ot <- function(type.long){
+  #####all types for gdal
+  ot.list <-c('Byte', 'UInt16', 'Int16', 'UInt32', 'Int32', 'Float32', 'Float64', 'CInt16', 'CInt32', 'CFloat32', 'CFloat64')
+
+  ###H5Types sorted
+  types.byte <- c("H5T_STD_B8BE","H5T_STD_B8LE","H5T_NATIVE_B8")
+  types.uint <- c("H5T_STD_U8BE","H5T_STD_U8LE","H5T_STD_U16BE","H5T_STD_U16LE","H5T_STD_U32BE",
+                  "H5T_STD_U32LE", "H5T_NATIVE_USHORT", "H5T_NATIVE_UINT",
+                  "H5T_NATIVE_UINT8", "H5T_NATIVE_UINT_LEAST8", "H5T_NATIVE_UINT_FAST8",
+                  "H5T_NATIVE_UINT16", "H5T_NATIVE_UINT_LEAST16", "H5T_NATIVE_UINT_FAST16",
+                  "H5T_NATIVE_UINT32","H5T_NATIVE_UINT_LEAST32","H5T_NATIVE_UINT_FAST32")
+  types.int <- c("H5T_STD_I8BE","H5T_STD_I8LE","H5T_STD_I16BE","H5T_STD_I16LE","H5T_STD_I32BE",
+                 "H5T_STD_I32LE", "H5T_NATIVE_SHORT", "H5T_NATIVE_INT",
+                 "H5T_NATIVE_INT8", "H5T_NATIVE_INT_LEAST8", "H5T_NATIVE_INT_FAST8", "H5T_NATIVE_INT16",
+                 "H5T_NATIVE_INT_LEAST16", "H5T_NATIVE_INT_FAST16", "H5T_NATIVE_INT32",
+                 "H5T_NATIVE_INT_LEAST32","H5T_NATIVE_INT_FAST32")
+  types.float <- c("H5T_IEEE_F32BE","H5T_IEEE_F32LE", "H5T_NATIVE_FLOAT", "H5T_NATIVE_DOUBLE", "H5T_NATIVE_LDOUBLE", "H5T_IEEE_F64BE","H5T_IEEE_F64LE")
+  types.notsupported <- c("H5T_NATIVE_CHAR","H5T_NATIVE_SCHAR","H5T_NATIVE_UCHAR","H5T_NATIVE_HADDR",
+                          "H5T_NATIVE_OPAQUE", "H5T_NATIVE_HSIZE","H5T_NATIVE_HSSIZE","H5T_NATIVE_HERR",
+                          "H5T_NATIVE_HBOOL", "H5T_C_S1", "H5T_FORTRAN_S1",
+                          #all types referring to 64 bit and not beeing float: not supported, because not available as ot type
+                          "H5T_NATIVE_B64", "H5T_STD_B64LE", "H5T_STD_B64BE","H5T_STD_U64BE",
+                          "H5T_NATIVE_UINT64","H5T_NATIVE_UINT_LEAST64","H5T_NATIVE_UINT_FAST64",
+                          "H5T_STD_U64LE", "H5T_STD_I64LE", "H5T_NATIVE_INT64",
+                          "H5T_NATIVE_INT_LEAST64", "H5T_NATIVE_INT_FAST64", "H5T_STD_I64BE",
+                          "H5T_NATIVE_ULONG", "H5T_NATIVE_ULLONG","H5T_NATIVE_LONG","H5T_NATIVE_LLONG",
+                          #only byte: 8 bit supportet
+                          "H5T_STD_B16BE","H5T_STD_B16LE","H5T_STD_B32BE","H5T_STD_B32LE","H5T_NATIVE_B16", "H5T_NATIVE_B32")
+
+
+  if(type.long %in% types.int){
+    ot = 'Int'
+  } else if(type.long %in% types.uint){
+    ot = 'UInt'
+  } else if(type.long %in% types.byte){
+    ot = 'Byte'
+  } else if(type.long %in% types.float){
+    ot = 'Float'
+  } else {
+    ot = NULL
+  }
+
+  if (!is.null(ot)){
+    #float and int
+    if (ot != 'Byte' & ot != 'UInt'){
+      #get bytes
+      bytes <- regmatches(type.long, gregexpr("[[:digit:]]+", type.long))[[1]][2]
+      #bytes are declared in type
+      if(! is.na(bytes)){
+        #catch bytes smaller 16
+        if(as.integer(bytes)<16){
+          #for int paste 16 (smalles bytes available)
+          if (ot == 'Int'){
+            ot <- paste0(ot, '16')
+          } else {
+            #for float paste 32 (smalles bytes available)
+            ot <- paste0(ot, '32')
+          }
+        } else{
+          #paste 'real' bytes if bigger 16
+          ot <- paste0(ot, bytes)
+        }
+      } else{
+        #bytes not declared in type
+        if (ot =='Float'){
+          if (stringr::str_detect(type.long, 'DOUBLE')){
+            ot <- paste0(ot, 64) #paste 64 bit for double
+          } else {
+            ot <- paste0(ot, 32) #paste 32 bit for float
+          }
+
+        } else {
+          if (stringr::str_detect(type.long, 'SHORT')){
+            ot <- paste0(ot, 16) #16 bit for short
+          } else {
+            ot <- paste0(ot, 32) #highest for other types
+          }
+        }
+      }
+    } else if (ot == 'UInt'){
+      # check UInt types
+      bytes <- regmatches(type.long, gregexpr("[[:digit:]]+", type.long))[[1]][2]
+      if(! is.na(bytes)){
+        if(as.integer(bytes)==16 | as.integer(bytes)==32){
+          ot <- paste0(ot, bytes)
+        } else if (as.integer(bytes)==8){
+          ot <- 'UInt16'
+        }
+      } else {
+        if (stringr::str_detect(type.long, 'SHORT')){
+          ot <- 'UInt16' #16 bit for short
+        } else {
+          ot <- 'UInt32' #highest for other types
+        }
+      }
+    }
+
+    if (! ot %in% ot.list){
+      ot = NULL
+    }
+  }
+
+  return(ot)
+}
+
+ebv_i_type_raster <- function(datanotation, byteorder){
+  types.raster <- c('LOG1S', 'INT1S', 'INT2S', 'INT4S', 'INT8S', 'INT1U', 'INT2U', 'FLT4S', 'FLT8S')
+  type.hdf <- ''
+  #define FLOAT type
+  if (substr(datanotation, 1,3)=='FLT'){
+    type.hdf <- paste0(type.hdf, 'H5T_IEEE_F')
+    #check bytes
+    if (substr(datanotation, 4,4)=='4'){
+      type.hdf <- paste0(type.hdf,'32')
+    }else if (substr(datanotation, 4,4)=='8'){
+      type.hdf <- paste0(type.hdf,'64')
+    }
+    #define INT type e.g. "H5T_STD_I8BE" OR "H5T_STD_U8BE"
+  } else if (substr(datanotation, 1,3)=='INT'){
+    #check signed or unsigned
+    if (substr(datanotation, 5,5)=='S'){
+      type.hdf <- paste0(type.hdf, 'H5T_STD_I')
+    } else if (substr(datanotation, 5,5)=='U') {
+      type.hdf <- paste0(type.hdf, 'H5T_STD_U')
+    }
+    #check bytes
+    if (substr(datanotation, 4,4)=='1'){
+      type.hdf <- paste0(type.hdf,'8')
+    }else if (substr(datanotation, 4,4)=='2'){
+      type.hdf <- paste0(type.hdf,'16')
+    } else if (substr(datanotation, 4,4)=='4'){
+      type.hdf <- paste0(type.hdf,'32')
+    }else if (substr(datanotation, 4,4)=='8'){
+      type.hdf <- paste0(type.hdf,'64')
+    }
+  }
+  #check LE or BE
+  if(byteorder=='little'){
+    type.hdf <- paste0(type.hdf,'LE')
+  } else if(byteorder=='big'){
+    type.hdf <- paste0(type.hdf,'BE')
+  }
+  #check if type  is created correctly
+  if (!type.hdf %in% rhdf5::h5const("H5T")){
+    warning(paste0('Output type for HDF file not created correctly: ', type.hdf,
+                   '\nPlease contact developer of package.\nInput: ',
+                   datanotation, ' and ', byteorder, '\nDefault type will be used: H5T_NATIVE_DOUBLE.'))
+    type.hdf <- 'H5T_NATIVE_DOUBLE'
+  }
+  return(type.hdf)
+}
 
 
 
