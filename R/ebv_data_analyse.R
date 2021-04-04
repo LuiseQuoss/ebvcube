6ecd7aeada32a5b975e3d4812c943dab8f28301a
Luise Quoß <luise.quoss@idiv.de>
2021-04-04 19:58:35 +0200

    added ebv_data_analyse, pending: roxygen comments

---
+++ R/ebv_data_analyse.R
@@ -0,0 +1,121 @@
+#' Title
+#'
+#' @param filepath
+#' @param datacubepath
+#' @param subset
+#' @param timestep
+#' @param at
+#' @param epsg
+#' @param numerical
+#' @param na.rm
+#'
+#' @return pending
+#' @export
+#' @seealso [ebvnetcdf::ebv_data_read_bb()] and [ebvnetcdf::ebv_data_read_shp()] for the usage of subsets
+#'
+#' @importFrom stats quantile
+#' @examples
+#' #pending
+ebv_data_analyse <- function(filepath, datacubepath, subset=NULL, timestep=1, at=TRUE, epsg = 4326, numerical=TRUE, na.rm=TRUE){
+  ####initial tests start ----
+  #are all arguments given?
+  if(missing(filepath)){
+    stop('Filepath argument is missing.')
+  }
+  if(missing(datacubepath)){
+    stop('Datacubepath argument is missing.')
+  }
+
+  #filepath check - nc
+  if (!file.exists(filepath)){
+    stop(paste0('File does not exist.\n', filepath))
+  }
+  if (!endsWith(filepath, '.nc')){
+    stop(paste0('File ending is wrong. File cannot be processed.'))
+  }
+
+  #file closed?
+  ebv_i_file_opened(filepath)
+
+  #variable check
+  hdf <- rhdf5::H5Fopen(filepath)
+  if (rhdf5::H5Lexists(hdf, datacubepath)==FALSE){
+    rhdf5::H5Fclose(hdf)
+    stop(paste0('The given variable is not valid:\n', datacubepath))
+  } else {
+    rhdf5::H5Fclose(hdf)
+  }
+
+  #get properties
+  prop <- ebv_properties(filepath, datacubepath)
+
+  #timestep check
+  #check if timestep is valid type
+  if (class(timestep)=='numeric'){
+    for (t in timestep){
+      if (! as.integer(t)==t){
+        stop('Timestep has to be an integer or a list of integers.')
+      }
+    }
+  } else {
+    stop('Timestep has to be of class numeric.')
+  }
+
+  #check timestep range
+  for (t in timestep){
+    max_time <- prop@spatial_information@dimensions[3]
+    min_time <- 1
+    if (t>max_time | t<min_time){
+      stop(paste0('Chosen timestep ', t, ' is out of bounds. Timestep range is ', min_time, ' to ', max_time, '.'))
+    }
+  }
+
+  #more checks are included in subset bb and subset shp
+
+  ####initial tests end ----
+
+  #process shp subset
+  if (is.null(subset)){
+    #process whole file + variable+ timestep
+    type.short <- ebv_i_type_r(prop@entity_information@type)
+    all <- HDF5Array::HDF5Array(filepath = filepath, name =datacubepath, as.sparse = T, type = type.short)
+    subset.array <- all[,,timestep]
+    #give fillvalue as nodata value
+    subset.array <- replace(subset.array, subset.array==prop@entity_information@fillvalue[1], c(NA))
+  }  else if(class(subset) == "numeric"){
+    #process bb subset
+    subset.raster <- ebv_data_read_bb(filepath, datacubepath, bb=subset, timestep=timestep, epsg=epsg)
+    #raster to array
+    subset.array <- as.array(subset.raster)
+    #less ram
+    rm(subset.raster)
+  } else if(endsWith(subset, '.shp')){
+    #process shp subset
+    subset.raster <- ebv_data_read_shp(filepath, datacubepath, shp=subset, timestep=timestep, at=at)
+    #raster to array
+    subset.array <- as.array(subset.raster)
+    #less ram
+    rm(subset.raster)
+  } else {
+    stop('Not processed. Check your given subset-indication.')
+  }
+
+  if(numerical){
+    #numerical stats
+    methods::setClass("EBV NetCDF numerical statistics", slots=list(min="numeric", q25 = "numeric", q50="numeric", mean ="numeric", q75 ="numeric", max="numeric", std ="numeric", n ="numeric", NAs ="numeric"))
+    n <- length(subset.array)
+    temp <- as.numeric(summary(array(subset.array)))
+    sd <- sd(subset.array, na.rm=na.rm)
+    stats <- methods::new("EBV NetCDF numerical statistics", min=temp[1], q25 = temp[2], q50=temp[3], mean =temp[4], q75 =temp[5], max=temp[6], std =sd, n =n, NAs =temp[7])
+  }else{
+    #categorical stats
+    methods::setClass("EBV NetCDF categorical statistics", slots=list(min="numeric", q25 = "numeric", q50="numeric",q75 ="numeric", max="numeric", n ="numeric", NAs ="numeric"))
+    n <- length(subset.array)
+    temp <- as.numeric(stats::quantile(subset.array, na.rm=na.rm))
+    NAs <- sum(is.na(subset.array))
+    stats <- methods::new("EBV NetCDF categorical statistics", min=temp[1], q25 = temp[2], q50=temp[3], q75 =temp[4], max=temp[5], n =n, NAs =NAs)
+  }
+
+  return(stats)
+
+}
