32bd5bc95b9af0e0cd8a7af0f552af02f3a55a59
Luise Quoß <luise.quoss@idiv.de>
2021-04-15 13:03:40 +0200

    minor change

--- R/ebvnetcdf-package.R
+++ R/ebvnetcdf-package.R
@@ -1,15 +1,16 @@
 #' Easy access of EVB NetCDFs
 #'
-#' @description This package can be used to easily access the data of the EBV
-#'   NetCDFs which can be downloaded from the \href{portal.geobon.org}{Geobon
-#'   Portal}. It also provides some basic visualization of the data.  Advanced
-#'   users can build their own NetCDFs with the EBV standard using this package.
 #'
-#' @details This package contains three main usecases: accessing the data and
-#'   visualising the data from the portal and creating your own data in the EBV
-#'   NetCDF standard. All function have a corresponding naming pattern:
-#'   ebv_data_ for data reading, ebv_plot_ for visualisation and ebv_netcdf_ for
-#'   creating a NetCDF.
+#' This package can be used to easily access the data of the EBV NetCDFs which
+#' can be downloaded from the \href{portal.geobon.org}{Geobon Portal}. It also
+#' provides some basic visualization of the data.  Advanced users can build
+#' their own NetCDFs with the EBV standard using this package.
+#'
+#' This package contains three main usecases: accessing the data and visualising
+#' the data from the portal and creating your own data in the EBV NetCDF
+#' standard. All function have a corresponding naming pattern: ebv_data_ for
+#' data reading, ebv_plot_ for visualisation and ebv_netcdf_ for creating a
+#' NetCDF.
 #'
 #' @docType package
 #' @name ebvnetcdf
