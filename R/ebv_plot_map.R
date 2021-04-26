c11ddfd5af9d16f743bedd99737c1f7ce1e8f92a
Luise Quoß <luise.quoss@idiv.de>
2021-04-26 12:58:55 +0200

    added classes option, checked the new (only!) arguments with checkmate

--- R/ebv_plot_map.R
+++ R/ebv_plot_map.R
@@ -13,6 +13,10 @@
 #'   top of the raster data. Disable by setting this option to FALSE.
 #' @param col.rev Default: TRUE. Set to FALSE if you want the color ramp to be
 #'   the other way around.
+#' @param classes Default: 5. Define the amount of classes (quantiles) of the
+#'   symbology.
+#' @param ignore.RAM Checks if there is enough space in your memory to read the
+#'   data. Can be switched off (set to TRUE).
 #' @param verbose Logical. Turn on all warnings by setting it to TRUE.
 #'
 #' @note Uses the country outlines data from the
@@ -26,7 +30,7 @@
 #' # file <- 'path/to/netcdf/file.nc'
 #' # datacubes <- ebv_datacubepaths(file)
 #' # ebv_plot_map(file, datacubes[1,1], 9)
-ebv_plot_map <- function(filepath, datacubepath, timestep=1, countries =TRUE, col.rev=TRUE, verbose=FALSE){
+ebv_plot_map <- function(filepath, datacubepath, timestep=1, countries =TRUE, col.rev=TRUE, classes = 5, ignore.RAM=FALSE, verbose=FALSE){
   #turn off local warnings if verbose=TRUE
   if(verbose){
     withr::local_options(list(warn = 0))
@@ -67,14 +71,8 @@ ebv_plot_map <- function(filepath, datacubepath, timestep=1, countries =TRUE, co
 
   #timestep check
   #check if timestep is valid type
-  if (class(timestep)=='numeric'){
-    for (t in timestep){
-      if (! as.integer(t)==t){
-        stop('Timestep has to be an integer or a list of integers.')
-      }
-    }
-  } else {
-    stop('Timestep has to be of class numeric.')
+  if (checkmate::checkInt(timestep)!=TRUE){
+    stop('The argument timestep must be of type "single integerish value"')
   }
 
   #check timestep range
@@ -84,6 +82,11 @@ ebv_plot_map <- function(filepath, datacubepath, timestep=1, countries =TRUE, co
     if (t>max_time | t<min_time){
       stop(paste0('Chosen timestep ', t, ' is out of bounds. Timestep range is ', min_time, ' to ', max_time, '.'))
     }
+  }
+
+  #check classes argument - single integer
+  if (checkmate::checkInt(classes)!=TRUE){
+    stop('The argument classes must be of type "single integerish value"')
   }
 
   # end initial tests ----
@@ -100,7 +103,7 @@ ebv_plot_map <- function(filepath, datacubepath, timestep=1, countries =TRUE, co
   results <- tryCatch(
     #try ----
     {
-      data.raster <- ebv_data_read(filepath, datacubepath, timestep = timestep, delayed = FALSE, raster=TRUE, verbose=verbose) #if this throws an error the data is going to plotted in lower res
+      data.raster <- ebv_data_read(filepath, datacubepath, timestep = timestep, delayed = FALSE, raster=TRUE, ignore.RAM=ignore.RAM, verbose=verbose) #if this throws an error the data is going to plotted in lower res
       data.all <- HDF5Array::HDF5Array(filepath = filepath, name = datacubepath, type = type.short)
       #mask out fillvalue ----
       data.all <- replace(data.all, data.all==fillvalue, c(NA))
@@ -125,7 +128,7 @@ ebv_plot_map <- function(filepath, datacubepath, timestep=1, countries =TRUE, co
         file.remove(temp.map)
       }
       #(filepath_src, datacubepath_src, resolution, outputpath, timestep = 1, method='average', return.raster=FALSE, overwrite = FALSE, ignore.RAM=FALSE)
-      data.raster <- ebv_data_change_res(filepath, datacubepath, c(1,1, 4326), temp.map, timestep, return.raster = TRUE, verbose=verbose)
+      data.raster <- ebv_data_change_res(filepath, datacubepath, c(1,1, 4326), temp.map, timestep, return.raster = TRUE, ignore.RAM=ignore.RAM, verbose=verbose)
       data.raster <- data.raster[[1]] #get first layer of brick --> class = raster
       data.all <- raster::as.array(data.raster) #use only one layer for quantile analysis
       results <- list(data.raster, data.all, temp.map)
@@ -137,12 +140,10 @@ ebv_plot_map <- function(filepath, datacubepath, timestep=1, countries =TRUE, co
   temp.map <- results[[3]]
 
   #get quantiles ----
-  s <- stats::quantile(data.all, probs = seq(0, 1, 0.2), na.rm=TRUE)
-  #s <- quantile(data.all[,,c(1,2,4,5,6,7,8,9,10,11,12,13,14,15)], probs=seq(0,1,0.2), na.rm=T)
-  #for predict
+  s <- stats::quantile(data.all, probs = seq(0, 1, (1/classes)), na.rm=TRUE)
 
   #check if quantile list values are unique
-  if(length(unique(s)) != 6){
+  if(length(unique(s)) != (classes+1)){
     message('Error while creating quantiles. Color Scale will be corrupted')
     s <- unique(s)
   }
@@ -150,10 +151,10 @@ ebv_plot_map <- function(filepath, datacubepath, timestep=1, countries =TRUE, co
   #get correct colors
   if (min(s)<0 & max(s)>0){
     diverging <- 'Blue-Red'
-    col.regions <- colorspace::diverging_hcl(5, palette = diverging, rev = col.rev) #"BluYl"
+    col.regions <- colorspace::diverging_hcl(classes, palette = diverging, rev = col.rev) #"BluYl"
   } else {
     single <- 'YlGn' # 'Greens'#
-    col.regions <- colorspace::sequential_hcl(5, palette = single, rev = col.rev) #"BluYl"
+    col.regions <- colorspace::sequential_hcl(classes, palette = single, rev = col.rev) #"BluYl"
   }
 
   #define display options ----
