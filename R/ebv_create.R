2cf7c3e9fbccb497cbdf86ed1208be95a5100d60
Luise Quoß <luise.quoss@idiv.de>
2021-09-01 12:59:12 +0200

    merge 3D and 4D

--- R/ebv_create.R
+++ R/ebv_create.R
@@ -1,4 +1,4 @@
-#' Create an EBV NetCDF
+#' Create an EBV netCDF
 #'
 #' @description Create the core structure of the EBV NetCDF based on the json
 #'   from the \href{https://portal.geobon.org/api-docs}{Geobon Portal API}. Data
@@ -23,8 +23,15 @@
 #'   outputfile defined by 'outputpath'.
 #' @param verbose Logical. Default: FALSE. Turn on all warnings by setting it to
 #'   TRUE.
-#'
-#' @note To check out the results take a look at your NetCDF file with
+#' @param resolution Numerical. Vector of two numerical values defining the
+#'   longitudinal and latitudinal resolution of the pixel: c(lon,lat).
+#' @param force_4D Logical. Default is TRUE. If the argument is TRUE, there will
+#'   be 4D cubes (lon, lat, time, entity) per metric. If this argument is
+#'   changed to FALSE, there will be 3D cubes (lon, lat, time) per entity (per
+#'   metric). So the latter yields a higher amount of cubes and does not bundle
+#'   all information per metric. In the future the standard will be restricted
+#'   to the 4D version. Recommendation: go with the 4D cubes!
+#' @note To check out the results take a look at your netCDF file with
 #'   \href{https://www.giss.nasa.gov/tools/panoply/}{Panoply} provided by the
 #'   NASA.
 #'
@@ -39,7 +46,7 @@
 #' #ebv_create(json, out, 3, fillvalue=-3.4E38)
 ebv_create <- function(jsonpath, outputpath, entities, epsg=4326,
                        extent= c(-180,180,-90,90), resolution=c(1,1),fillvalue = NULL,
-                       prec = 'double', overwrite=FALSE,verbose=FALSE){
+                       prec = 'double', force_4D=TRUE, overwrite=FALSE,verbose=FALSE){
   # start initial tests ----
   # ensure file and all datahandles are closed on exit
   withr::defer(
@@ -67,16 +74,6 @@ ebv_create <- function(jsonpath, outputpath, entities, epsg=4326,
     }
   )
   withr::defer(
-    if(exists('aid')){
-      if(rhdf5::H5Iis_valid(aid)==TRUE){rhdf5::H5Aclose(aid)}
-    }
-  )
-  withr::defer(
-    if(exists('sid')){
-      if(rhdf5::H5Iis_valid(sid)==TRUE){rhdf5::H5Sclose(sid)}
-    }
-  )
-  withr::defer(
     if(exists('nc')){
       tryCatch(l <- utils::capture.output(ncdf4::nc_close(nc)))
     }
@@ -88,6 +85,9 @@ ebv_create <- function(jsonpath, outputpath, entities, epsg=4326,
   }
   if(missing(outputpath)){
     stop('Outputpath argument is missing.')
+  }
+  if(missing(entities)){
+    stop('Entities argument is missing.')
   }
 
   #turn off local warnings if verbose=TRUE
@@ -103,6 +103,9 @@ ebv_create <- function(jsonpath, outputpath, entities, epsg=4326,
   #check logical arguments
   if(checkmate::checkLogical(overwrite, len=1, any.missing=F) != TRUE){
     stop('overwrite must be of type logical.')
+  }
+  if(checkmate::checkLogical(force_4D, len=1, any.missing=F) != TRUE){
+    stop('force_4D must be of type logical.')
   }
 
   #check if json exists
@@ -190,6 +193,10 @@ ebv_create <- function(jsonpath, outputpath, entities, epsg=4326,
     }
   }
 
+  #check resolution
+  if (checkmate::checkNumeric(resolution, len = 2) != TRUE){
+    stop('resolution needs to be a list of 2 numeric values.')
+  }
 
   # end initial tests ----
 
@@ -323,28 +330,53 @@ ebv_create <- function(jsonpath, outputpath, entities, epsg=4326,
   time_dim <- ncdf4::ncdim_def('time', 'days since 1860-01-01 00:00:00.0' , timesteps, unlim = T)
   entity_dim <- ncdf4::ncdim_def('entity', '', vals = 1:entities_no, create_dimvar=FALSE)
 
-  # create list of vars ----
-  var_list <- c()
-  # 1. metric, no scenario
-  if(scenarios_no==0){
-    for (j in 1:(metrics_no)){
-      #all entities for that metric
-      for (ent in entity.list){
-        var_list <- c(var_list, paste0('metric_', as.character(j), '/', ent))
+  # create list of vars 3D ----
+  if(force_4D==FALSE){
+    var_list <- c()
+    # 1. metric, no scenario
+    if(scenarios_no==0){
+      for (j in 1:(metrics_no)){
+        #all entities for that metric
+        for (ent in entity.list){
+          var_list <- c(var_list, paste0('metric_', as.character(j), '/', ent))
+        }
+      }
+      #2. scenario and metric (entities are not relevant)
+    } else {
+      for (i in 1:(scenarios_no)){
+        for (j in 1:(metrics_no)){
+          #add entities
+          for (ent in entity.list){
+            var_list <- c(var_list, paste0('scenario_', as.character(i), '/metric_', as.character(j), '/',ent))
+          }
+        }
       }
     }
-    #2. scenario and metric (entities are not relevant)
-  } else {
-    for (i in 1:(scenarios_no)){
+  } else{
+    # create list of vars 4D----
+    var_list <- c()
+    # 1. metric, no scenario
+    if(scenarios_no==0){
       for (j in 1:(metrics_no)){
-        #add entities
-        for (ent in entity.list){
-          var_list <- c(var_list, paste0('scenario_', as.character(i), '/metric_', as.character(j), '/',ent))
+        #add ebv_cube
+        var_list <- c(var_list, paste0('metric_', as.character(j), '/ebv_cube'))
+      }
+      #2. scenario and metric (entities are not relevant)
+    } else {
+      for (i in 1:(scenarios_no)){
+        #create scenario group
+        ending.s <- as.character(i)
+        for (j in 1:(metrics_no)){
+          #create metric group
+          ending.m <- as.character(j)
+          #add ebv_cube
+          var_list <- c(var_list, paste0('scenario_', ending.s, '/metric_', ending.m, '/ebv_cube'))
         }
       }
     }
   }
 
+  #get units of metric ----
   units <- c()
   for (j in 1:(metrics_no)){
     #metric units list
@@ -354,26 +386,51 @@ ebv_create <- function(jsonpath, outputpath, entities, epsg=4326,
   var_list_nc <- list()
   enum = 1
 
-    # create all vars ---
-  if (!is.null(fillvalue)){
-    for (var in var_list){
-      metric.str <- stringr::str_split(var, '/')[[1]][stringr::str_detect(stringr::str_split(var, '/')[[1]], 'metric')]
-      metric.digit <- as.numeric(regmatches(metric.str, gregexpr("[[:digit:].]+", metric.str))[[1]])
-      name <- paste0('var', enum)
-      assign(name, ncdf4::ncvar_def(var, units[metric.digit], dim= list(lon_dim, lat_dim, time_dim), missval=fillvalue, compression=2, prec=prec, verbose=verbose))
-      var_list_nc[[enum]] <- eval(parse(text=name))
-      enum = enum +1
+
+  # create all vars 3D ----
+  if(force_4D==FALSE){
+    if (!is.null(fillvalue)){
+      for (var in var_list){
+        metric.str <- stringr::str_split(var, '/')[[1]][stringr::str_detect(stringr::str_split(var, '/')[[1]], 'metric')]
+        metric.digit <- as.numeric(regmatches(metric.str, gregexpr("[[:digit:].]+", metric.str))[[1]])
+        name <- paste0('var', enum)
+        assign(name, ncdf4::ncvar_def(var, units[metric.digit], dim= list(lon_dim, lat_dim, time_dim), missval=fillvalue, compression=2, prec=prec, verbose=verbose))
+        var_list_nc[[enum]] <- eval(parse(text=name))
+        enum = enum +1
+      }
+    } else {
+      for (var in var_list){
+        metric.str <- stringr::str_split(var, '/')[[1]][stringr::str_detect(stringr::str_split(var, '/')[[1]], 'metric')]
+        metric.digit <- as.numeric(regmatches(metric.str, gregexpr("[[:digit:].]+", metric.str))[[1]])
+        name <- paste0('var', enum)
+        assign(name, ncdf4::ncvar_def(var, units[metric.digit], dim= list(lon_dim, lat_dim, time_dim), compression=2, prec=prec, verbose=verbose))
+        var_list_nc[[enum]] <- eval(parse(text=name))
+        enum = enum +1
+      }
     }
-  } else {
-    for (var in var_list){
-      metric.str <- stringr::str_split(var, '/')[[1]][stringr::str_detect(stringr::str_split(var, '/')[[1]], 'metric')]
-      metric.digit <- as.numeric(regmatches(metric.str, gregexpr("[[:digit:].]+", metric.str))[[1]])
-      name <- paste0('var', enum)
-      assign(name, ncdf4::ncvar_def(var, units[metric.digit], dim= list(lon_dim, lat_dim, time_dim), compression=2, prec=prec, verbose=verbose))
-      var_list_nc[[enum]] <- eval(parse(text=name))
-      enum = enum +1
+  }else{
+  # create all vars 4D ----
+    if (!is.null(fillvalue)){
+      for (var in var_list){
+        metric.str <- stringr::str_split(var, '/')[[1]][stringr::str_detect(stringr::str_split(var, '/')[[1]], 'metric')]
+        metric.digit <- as.numeric(regmatches(metric.str, gregexpr("[[:digit:].]+", metric.str))[[1]])
+        name <- paste0('var', enum)
+        assign(name, ncdf4::ncvar_def(var, units[metric.digit], dim= list(lon_dim, lat_dim, time_dim, entity_dim), missval=fillvalue, compression=2, prec=prec, verbose=verbose))
+        var_list_nc[[enum]] <- eval(parse(text=name))
+        enum = enum +1
+      }
+    } else {
+      for (var in var_list){
+        metric.str <- stringr::str_split(var, '/')[[1]][stringr::str_detect(stringr::str_split(var, '/')[[1]], 'metric')]
+        metric.digit <- as.numeric(regmatches(metric.str, gregexpr("[[:digit:].]+", metric.str))[[1]])
+        name <- paste0('var', enum)
+        assign(name, ncdf4::ncvar_def(var, units[metric.digit], dim= list(lon_dim, lat_dim, time_dim, entity_dim), compression=2, prec=prec, verbose=verbose))
+        var_list_nc[[enum]] <- eval(parse(text=name))
+        enum = enum +1
+      }
     }
   }
+
 
   #add crs variable ----
   var_list_nc[[enum]] <- ncdf4::ncvar_def('crs', '', dim= list(), compression=2, prec='char', verbose=verbose)
@@ -464,7 +521,6 @@ ebv_create <- function(jsonpath, outputpath, entities, epsg=4326,
   for (i in 1:length(global.att)){
     att.txt <- eval(parse(text = paste0('json$', global.att[i][[1]])))
     att.txt <- paste(att.txt, collapse = '; ')
-    #print(att.txt)
     ebv_i_char_att(hdf, names(global.att[i]), att.txt)
   }
 
@@ -616,17 +672,30 @@ ebv_create <- function(jsonpath, outputpath, entities, epsg=4326,
     }
   }
 
-  #add entity attributes ----
-  enum <-1
-  for(var in var_list){
-    did <- rhdf5::H5Dopen(hdf, var)
-    ebv_i_char_att(did, 'grid_mapping', '/crs')
-    ebv_i_char_att(did, 'coordinate', '/entities')
-    ebv_i_char_att(did, 'coverage_content_type', paste0(json$coverage_content_type, collapse='; '))
-    ebv_i_char_att(did, 'standard_name', entity_csv[enum,1])
-    #close dh
-    rhdf5::H5Dclose(did)
-    enum <- enum +1
+  #add entity attributes 3D ----
+  if(force_4D==FALSE){
+    enum <-1
+    for(var in var_list){
+      did <- rhdf5::H5Dopen(hdf, var)
+      ebv_i_char_att(did, 'grid_mapping', '/crs')
+      ebv_i_char_att(did, 'coordinate', '/entities')
+      ebv_i_char_att(did, 'coverage_content_type', paste0(json$coverage_content_type, collapse='; '))
+      ebv_i_char_att(did, 'standard_name', entity_csv[enum,1])
+      #close dh
+      rhdf5::H5Dclose(did)
+      enum <- enum +1
+    }
+  }else{
+  #add entity attributes 4D ----
+    enum <-1
+    for(var in var_list){
+      did <- rhdf5::H5Dopen(hdf, var)
+      ebv_i_char_att(did, 'grid_mapping', '/crs')
+      ebv_i_char_att(did, 'coordinate', '/entities')
+      ebv_i_char_att(did, 'coverage_content_type', paste0(json$coverage_content_type, collapse='; '))
+      #close dh
+      rhdf5::H5Dclose(did)
+    }
   }
 
   # close file  ----
