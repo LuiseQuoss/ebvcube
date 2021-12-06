8daba88e8fcdfa63db397e9d7be1ba4a721b0021
Luise Quoß <luise.quoss@idiv.de>
2021-12-06 10:55:51 +0100

    minor

--- R/ebv_create.R
+++ R/ebv_create.R
@@ -147,7 +147,8 @@ ebv_create <- function(jsonpath, outputpath, entities, epsg=4326,
   #check if epsg is valid
   if(stringr::str_detect(epsg, 'ESRI')){
     crs <- gdalUtils::gdalsrsinfo(epsg)
-  } else if (checkmate::checkIntegerish(epsg) != TRUE){
+    print(crs)
+  } else if (!stringr::str_detect(epsg, 'ESRI') & checkmate::checkIntegerish(epsg) != TRUE){
     stop('epsg must be of type integer.')
   } else {
     crs <- gdalUtils::gdalsrsinfo(paste0('EPSG:',epsg))
