60179aa7f4461500e30671d97345b642bd63f5d3
Luise Quoß <luise.quoss@idiv.de>
2022-01-04 15:07:19 +0100

    minor

--- R/ebv_read_bb.R
+++ R/ebv_read_bb.R
@@ -37,7 +37,7 @@
 #' #set path to EBV netCDF
 #' file <- system.file(file.path("extdata","cSAR_idiv_v1.nc"), package="ebvcube")
 #' #get all datacubepaths of EBV netCDF
-#' datacubes <- ebv_datacubepaths(file)
+#' # datacubes <- ebv_datacubepaths(file)
 #'
 #' #set outputpath
 #' out <- file.path(system.file(package='ebvcube'),"extdata","subset_bb.tif")
@@ -45,7 +45,6 @@
 #' bb_wgs84 <- c(5,15,47,55)
 #' bb_utm32 <- c(271985, 941837, 5232640, 6101151)
 #'
-#' print(gdalUtils::gdalsrsinfo(paste0("EPSG:", 32632)))
 #'
 #' #read bb (based on EPSG 4326) - return Raster
 #' # cSAR.germany <- ebv_read_bb(filepath = file, datacubepath = datacubes[1,1],
