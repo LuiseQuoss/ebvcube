00971f2d2e3718ea31f686f0839daaa26a4601d6
Luise Quoß <luise.quoss@idiv.de>
2022-08-23 13:02:14 +0200

    data to SpatVector - description changed

--- R/data.R
+++ R/data.R
@@ -1,7 +1,7 @@
 #' Simple outlines of world countries
 #'
 #'
-#' @format A Spatial Polygons Data Frame with 177 elements
+#' @format A SpatVector (terra) with 177 elements
 #' @source Data downloaded from
 #'   \href{https://www.naturalearthdata.com/downloads/110m-cultural-vectors/110m-admin-0-countries/}{Natural
 #'   Earth}. Used version 4.0.0 and reduced attributes.
