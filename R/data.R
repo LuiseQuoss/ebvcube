1c334a0028b5a9715b616cce179ad335de4e8fe2
Luise Quoß <luise.quoss@idiv.de>
2022-08-24 08:55:41 +0200

    save data as data.frame (terra data is only pointer)

--- R/data.R
+++ R/data.R
@@ -1,8 +1,8 @@
 #' Simple outlines of world countries
 #'
 #'
-#' @format A SpatVector (terra) with 177 elements
+#' @format A data.frame with 177 elements and geometry as WKT
 #' @source Data downloaded from
 #'   \href{https://www.naturalearthdata.com/downloads/110m-cultural-vectors/110m-admin-0-countries/}{Natural
 #'   Earth}. Used version 4.0.0 and reduced attributes.
-"world_boundaries"
+#"world_boundaries"
