87889d0a803babd39e62155bdbb9858f956babdb
Luise Quoß <luise.quoss@idiv.de>
2022-08-29 13:23:25 +0200

    minor

--- R/ebv_trend.R
+++ R/ebv_trend.R
@@ -146,7 +146,7 @@ ebv_trend <- function(filepath, datacubepath, entity=NULL, method='mean',
   time <- prop@spatial$dimensions[3]
   timevalues <- prop@temporal$timesteps_natural
   #derive years
-  if(prop@temporal$resolution != 'irregular'){
+  if(grepl('^P\\d{4}-?\\d{0,2}-?\\d{0,2}$', prop@temporal$resolution)){
     if(stringr::str_split(prop@temporal$resolution, '-')[[1]][2]=='00' &
        stringr::str_split(prop@temporal$resolution, '-')[[1]][3]=='00'){
       timevalues <- format(as.Date(timevalues, format='%Y-%m-%d'), '%Y')
