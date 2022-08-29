bef1b3ed6211a2c77f3ffe9e4b1d45cc58eda76c
Luise Quoß <luise.quoss@idiv.de>
2022-08-29 11:28:01 +0200

    render data documentation

--- R/data.R
+++ R/data.R
@@ -5,4 +5,6 @@
 #' @source Data downloaded from
 #'   \href{https://www.naturalearthdata.com/downloads/110m-cultural-vectors/110m-admin-0-countries/}{Natural
 #'   Earth}. Used version 4.0.0 and reduced attributes.
-#"world_boundaries"
+#' @docType data
+"world_boundaries"
+
