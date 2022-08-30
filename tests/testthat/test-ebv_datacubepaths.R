e503487ae8aceb98e7baeab948a555f4503f529b
Luise Quoß <luise.quoss@idiv.de>
2022-08-30 09:24:40 +0200

    add ebv_datacubepath test for scenario-based netcdf

--- tests/testthat/test-ebv_datacubepaths.R
+++ tests/testthat/test-ebv_datacubepaths.R
@@ -6,3 +6,21 @@ test_that("datacubepaths martins_comcom_id1_20220208_v1.nc", {
   datacubes <- ebv_datacubepaths(file)
   expect_equal(datacubes, result)
 })
+
+test_that("datacubepaths pereira_csar_bes_sim_20220830_4d.nc", {
+  file <- system.file(file.path("extdata","pereira_csar_bes_sim_20220830_4d.nc"), package="ebvcube")
+  result <- data.frame(c('scenario_1/metric_1/ebv_cube', 'scenario_1/metric_2/ebv_cube',
+                         'scenario_1/metric_3/ebv_cube',
+                         'scenario_2/metric_1/ebv_cube', 'scenario_2/metric_2/ebv_cube',
+                         'scenario_2/metric_3/ebv_cube',
+                         'scenario_3/metric_1/ebv_cube', 'scenario_3/metric_2/ebv_cube',
+                         'scenario_3/metric_3/ebv_cube'))
+  result <- cbind(result, c('SSP1-RCP1.5 LU','SSP1-RCP1.5 LU','SSP1-RCP1.5 LU',
+                            'SSP3-RCP6.0 LU','SSP3-RCP6.0 LU','SSP3-RCP6.0 LU',
+                            'SSP5-RCP8.5 LU', 'SSP5-RCP8.5 LU', 'SSP5-RCP8.5 LU'))
+  result <- cbind(result, c('Species richness (S)','Relative species richness change (Delta_S)',
+                            'Diversity-weighted relative species richness change (Delta_SS)'))
+  colnames(result) <- c('datacubepaths', 'scenario_names', 'metric_names')
+  datacubes <- ebv_datacubepaths(file)
+  expect_equal(datacubes, result)
+})
