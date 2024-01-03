6a15c7475451ffb31aaddd18a7a8cd679c7373c0
Luise Quoß <luise.quoss@idiv.de>
2024-01-03 11:42:55 +0100

    ebv_download: return path of netCDF file also if it is already downlaoded (and overwrite=F)

--- R/ebv_download.R
+++ R/ebv_download.R
@@ -163,7 +163,10 @@ ebv_download <- function(id=NULL,
 
     #check if netCDF file already exists
     if(file.exists(file.path(outputdir, name_nc)) & overwrite==FALSE ){
-      stop('NetCDF already downloaded to this directory. Set overwrite to TRUE to replace the older version.')
+      if(verbose){
+        print('NetCDF already downloaded to this directory. Set overwrite to TRUE to replace the older version.')
+      }
+      return(file.path(outputdir, name_nc))
     }
 
     #download netcdf
