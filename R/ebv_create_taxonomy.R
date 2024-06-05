e485ab7a9fc2d00cab32ad220255d569d8d56891
Luise Quoß <luise.quoss@idiv.de>
2024-06-05 11:42:17 +0200

    add example for ebv_create_taxonomy

--- R/ebv_create_taxonomy.R
+++ R/ebv_create_taxonomy.R
@@ -63,6 +63,20 @@
 #'@importFrom utils head
 #'
 #' @examples
+#' #set path to JSON file
+#' json <- system.file(file.path("extdata/testdata","5.json"), package="ebvcube")
+#' #set output path of the new EBV netCDF
+#' out <-  tempfile(fileext='.nc')
+#' #set path to the csv holding the taxonomy names
+#' taxonomy <- file.path(system.file(package='ebvcube'),"extdata/testdata","id5_entities.csv")
+#'
+#' #create new EBV netCDF with taxonomy
+#' \dontrun{
+#' ebv_create_taxonomy(jsonpath = json, outputpath = out, taxonomy = taxonomy,
+#'            fillvalue = -127, resolution = c(0.25, 0.25), verbose = FALSE)
+#' #remove file
+#' file.remove(out)
+#' }
 ebv_create_taxonomy <- function(jsonpath, outputpath, taxonomy, lsid=FALSE,
                                 epsg = 4326, extent = c(-180,180,-90,90), resolution = c(1,1),
                                 timesteps = NULL, fillvalue = NULL, prec = 'double',
