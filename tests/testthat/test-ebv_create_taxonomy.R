72f1f1d98cc8e1681e2f635b271b916af9590ce3
Luise Quoß <luise.quoss@idiv.de>
2024-06-05 11:26:29 +0200

    ebv_create_taxonomy: finalize test (add test data)

--- tests/testthat/test-ebv_create_taxonomy.R
+++ tests/testthat/test-ebv_create_taxonomy.R
@@ -1,19 +1,19 @@
-test_that("test ebv_create_taxonomy", {
+test_that("test ebv_create_taxonomy no lsid", {
   #test ebv_create_taxonomy without lsid ----
 
   #basic paths
-  root <-'I:\\biocon\\Quoss_Luise\\netcdf\\taxonomy'
-  json <- file.path(root, '35.json')
-  taxonomy <- file.path(root, 'List_reptiles_20052022_function.csv')
+  root <- system.file(file.path("extdata/testdata"), package="ebvcube")
+  json <- file.path(root, '5.json')
+  taxonomy <- file.path(root, 'id5_entities.csv')
   file <- tempfile(fileext='.nc')
 
   #spatial info
-  epsg = 4326
-  extent = c(-180.0000000000000000,179.9999999999999432,-89.9999999999999716, 90.0000000000000000)
-  resolution = c(0.4166666666666666297, 0.4166666666666666297)
-  fillvalue = 0
-  prec = 'byte'
-  sep=';'
+  extent <- c(-180, 180, -90, 90)
+  res <- c(0.25, 0.25)
+  fillvalue <- -127
+  prec <- 'float'
+  epsg <- 4326
+  sep <- ','
 
   #create empty file
   expect_silent(ebv_create_taxonomy(jsonpath = json,
@@ -35,11 +35,9 @@ test_that("test ebv_create_taxonomy", {
 
   #entity_list
   entity_list.id <- rhdf5::H5Dopen(hdf, 'entity_list')
-  expect_equal(trimws(paste0(entity_list.id[1,1,], collapse = '')), "")
-  expect_equal(trimws(paste0(entity_list.id[2,1,], collapse = '')), "Ablepharus")
-  expect_equal(trimws(paste0(entity_list.id[3,1,], collapse = '')), "SCINCIDAE")
-  expect_equal(trimws(paste0(entity_list.id[4,1,], collapse = '')), "SAURIA")
-  expect_equal(trimws(paste0(entity_list.id[5,1,], collapse = '')), "Least Concern")
+  expect_equal(trimws(paste0(entity_list.id[1,1,], collapse = '')), "Hipposideros curtus")
+  expect_equal(trimws(paste0(entity_list.id[2,1,], collapse = '')), "Hipposideridae")
+  expect_equal(trimws(paste0(entity_list.id[3,1,], collapse = '')), "Chiroptera")
   rhdf5::H5Dclose(entity_list.id)
 
   #no lsid created
@@ -47,11 +45,73 @@ test_that("test ebv_create_taxonomy", {
 
   #taxon level names
   entity_levels.id <- rhdf5::H5Dopen(hdf, 'entity_levels')
-  expect_equal(trimws(paste0(entity_levels.id[1,], collapse = '')), "Species")
-  expect_equal(trimws(paste0(entity_levels.id[2,], collapse = '')), "Genus")
-  expect_equal(trimws(paste0(entity_levels.id[3,], collapse = '')), "Family")
-  expect_equal(trimws(paste0(entity_levels.id[4,], collapse = '')), "Order")
-  expect_equal(trimws(paste0(entity_levels.id[5,], collapse = '')), "RedlistCategory")
+  expect_equal(trimws(paste0(entity_levels.id[1,], collapse = '')), "binomial")
+  expect_equal(trimws(paste0(entity_levels.id[2,], collapse = '')), "family")
+  expect_equal(trimws(paste0(entity_levels.id[3,], collapse = '')), "order")
+  rhdf5::H5Dclose(entity_levels.id)
+
+  rhdf5::H5Fclose(hdf)
+
+  #remove file
+  file.remove(file)
+
+
+})
+
+
+test_that("test ebv_create_taxonomy with lsid", {
+  #test ebv_create_taxonomy with lsid ----
+
+  #basic paths
+  root <- system.file(file.path("extdata/testdata"), package="ebvcube")
+  json <- file.path(root, '5.json')
+  taxonomy <- file.path(root, 'id5_taxid.csv')
+  file <- tempfile(fileext='.nc')
+
+  #spatial info
+  extent <- c(-180, 180, -90, 90)
+  res <- c(0.25, 0.25)
+  fillvalue <- -127
+  prec <- 'float'
+  epsg <- 4326
+  sep <- ','
+
+  #create empty file
+  expect_silent(ebv_create_taxonomy(jsonpath = json,
+                                    outputpath = file,
+                                    taxonomy = taxonomy,
+                                    lsid=TRUE,
+                                    epsg = epsg,
+                                    extent = extent,
+                                    timesteps = '2020-12-31',
+                                    fillvalue = fillvalue,
+                                    prec = prec,
+                                    sep = sep,
+                                    force_4D = TRUE,
+                                    overwrite = TRUE,
+                                    verbose = FALSE))
+
+  #test the elements of the taxonomy
+  hdf <- rhdf5::H5Fopen(file)
+
+  #entity_list
+  entity_list.id <- rhdf5::H5Dopen(hdf, 'entity_list')
+  expect_equal(trimws(paste0(entity_list.id[1,2,], collapse = '')), "Microcebus rufus")
+  expect_equal(trimws(paste0(entity_list.id[2,2,], collapse = '')), "Cheirogaleidae")
+  expect_equal(trimws(paste0(entity_list.id[3,2,], collapse = '')), "Primates")
+  rhdf5::H5Dclose(entity_list.id)
+
+  #no lsid created
+  expect_equal(rhdf5::H5Lexists(hdf, 'entity_lsid'), TRUE)
+  entity_lsid.id <- rhdf5::H5Dopen(hdf, 'entity_lsid')
+  expect_equal(trimws(paste0(entity_lsid.id[1,], collapse = '')), "10125")
+  rhdf5::H5Dclose(entity_lsid.id)
+
+  #taxon level names
+  entity_levels.id <- rhdf5::H5Dopen(hdf, 'entity_levels')
+  expect_equal(trimws(paste0(entity_levels.id[1,], collapse = '')), "binomial")
+  expect_equal(trimws(paste0(entity_levels.id[2,], collapse = '')), "family")
+  expect_equal(trimws(paste0(entity_levels.id[3,], collapse = '')), "order")
   rhdf5::H5Dclose(entity_levels.id)
 
   rhdf5::H5Fclose(hdf)
